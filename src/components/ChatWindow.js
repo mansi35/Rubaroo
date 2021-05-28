622a9765e71d3303e1cf02a9e4076f76cf41f2b6
Vaibhavi1707 <vaibhavilokegaonkar@gmail.com>
2021-05-28 23:11:53 +0530

    Completed chat

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -7,16 +7,33 @@ import ReceiverChatBubble from "./ReceiverChatBubble";
 import MoodIcon from '@material-ui/icons/Mood';
 import MicNoneIcon from '@material-ui/icons/MicNone';
 import SendIcon from '@material-ui/icons/Send';
+import SportsEsportsOutlinedIcon from '@material-ui/icons/SportsEsportsOutlined';
+import VideoCallOutlinedIcon from '@material-ui/icons/VideoCallOutlined';
 
 function ChatWindow() {
     return (
         <div className = "chatWindow">
             <div className = "chat__header">
+                <div className = "pic__name">
                 <Avatar src = "https://www.clipartkey.com/mpngs/m/106-1068026_clip-art-old-woman-old-woman-face-clipart.png" />
                 <h3>Granny</h3>
+                </div>
+
+                <div className = "chat__trivia">
+                    <div className = "chat__videocall">
+                        <VideoCallOutlinedIcon />
+                    </div>
+
+                    <div className = "chat__gaming">
+                        <SportsEsportsOutlinedIcon />
+                    </div>
+                </div>
             </div>
 
             <div className = "chat__body">
+                {/* <div className = "chat__time">
+                    <p>Today</p>
+                </div> */}
 
                 <div className = "chat__content">
                     <SenderChatBubble message = "Hello How are you?" />
