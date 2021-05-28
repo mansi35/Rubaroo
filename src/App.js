683e0f5f0c7fb07d21210d1de6b184a42ac32237
Mansi Sharma <mansi.isha.mansi@gmail.com>
2021-05-28 14:10:44 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import LeftSideNavbar from './components/LeftSidebar';
 import RightSideNavbar from './components/RightSideNavbar';
 import Feed from './components/Feed';
 import OrganizationSearch from './pages/OrganizationSearch';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <OrganizationSearch />
-      <LeftSideNavbar />
-      <Feed />
-      <RightSideNavbar />
+      <Router>
+        <Switch>
+          <Route path="/institutions">
+            <OrganizationSearch />
+          </Route>
+          <Route path="/dashboard">
+            <LeftSideNavbar />
+            <Feed />
+            <RightSideNavbar />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
