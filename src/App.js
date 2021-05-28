f2fccf6a2561ee335dd137f4386ea5faa423a2be
Mansi Sharma <mansi.isha.mansi@gmail.com>
2021-05-28 12:24:49 +0530

    add initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import OrganizationSearch from './pages/OrganizationSearch';
+
+function App() {
+  return (
+    <div className="app">
+      <OrganizationSearch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/OrganizationSearch.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function OrganizationSearch() {
+    return (
+        <div className="organizationSearch">
+            <div className="row" style={{ padding: 0, margin: 0 }}>
+                <div className="organizationSearch__sidebar col-md-3">
+                    
+                </div>
+                <div className="organizationSearch__body col-md-9">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OrganizationSearch
