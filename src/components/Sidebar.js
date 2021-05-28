92d79a8a10976aba202e3365794e434383bbacc3
Vaibhavi1707 <vaibhavilokegaonkar@gmail.com>
2021-05-28 12:31:20 +0530

    Files shifted

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './SideNavbar.css';
+import './NavbarOption';
 
-function Sidebar() {
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+
+
+function SideNavbar() {
     return (
-        <div>
-            
+        <div className = "sideNavbar">
+            <h1>Rubaroo</h1> 
+            <NavbarOption Icon = {}/>
+            <NavbarOption />
         </div>
     )
 }
 
-export default Sidebar
+export default SideNavbar
